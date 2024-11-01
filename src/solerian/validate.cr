7742f4d2cb0aac19e7b293f2092a01a0f9fccdcd
Emilia Kond <emilia@rymiel.space>
2024-11-01 11:00:57 -0300

    update syllable phonotactics

--- src/solerian/validate.cr
+++ src/solerian/validate.cr
@@ -2,7 +2,7 @@
 module Solerian::Validation
   ONSET     = /(?<onset>sk|(?:[tdkg](?:[lr]|s)|(?:st|[mftdnrslɲjkgx]))?)/
   NUCLEUS   = /(?<nucleus>[aeiouəɨ])/
-  CODA_BODY = /(?:(?:x[lrs])|s[tdkg]|[lr](?:s|[tdkg]|[nm])|[tdkg]s|[nm](?:s|[tdkg])|(?:st|[mftdnrslɲjkgx]))?/
+  CODA_BODY = /(?:(?:[xj][lrs])|s[tdkg]|[lr](?:s|[tdkg]|[nm])|[tdkg]s|[nm](?:s|[tdkg])|(?:st|[mftdnrslɲjkgx]))?/
   CODA      = "(?<coda>(?=\\g<onset>\\g<nucleus>|$)|(?:st|[mftdnrslɲjkgx])(?=\\g<onset>\\g<nucleus>|$)|#{CODA_BODY})"
   SYLLABLES = /^(#{ONSET}#{NUCLEUS}#{CODA}(?=\g<onset>|$))+/
 
